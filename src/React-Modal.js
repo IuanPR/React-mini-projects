6f5e8af254849d3591c5616f42b1bb3738a48181
IuanPR <103894461+IuanPR@users.noreply.github.com>
2022-12-22 21:43:40 +0300

    Update modal

--- src/React-Modal.js
+++ src/React-Modal.js
@@ -1,38 +1,22 @@
 import React from "react";
-// Modal page on React for abrupt opening/closing
-class ModalPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
-    };
-  }
-  OpenBlock() {
-    this.setState({
-      open: true,
-    });
-  }
-  CloseBlock() {
-    this.setState({
-      open: false,
-    });
-  }
-  render() {
-    return (
-      <div className="parent-block">
-        <button onClick={() => this.OpenBlock()}>Open Modal</button>
-        {this.state.open && (
-          <div className="overlay">
-            <div className="modal">
-              <h3>Hello</h3>
-              <p>This is a modal page</p>
-              <button onClick={() => this.CloseBlock()}>Close Modal</button>
-            </div>
+import { useState } from "react";
+export default function ModalPage() {
+  const [visible, setVisible] = useState(false);
+  const onClickButton = () => {
+    setVisible((currentValue) => !currentValue);
+  };
+  return (
+    <div className="parent-block">
+      <button onClick={onClickButton}>Open Modal</button>
+      {visible && (
+        <div className="overlay">
+          <div className="modal">
+            <h3>Hello</h3>
+            <p>This is a modal page</p>
+            <button onClick={onClickButton}>Close Modal</button>
           </div>
-        )}
-      </div>
-    );
-  }
+        </div>
+      )}
+    </div>
+  );
 }
-// Someday I will do it with animation
-export default ModalPage;
